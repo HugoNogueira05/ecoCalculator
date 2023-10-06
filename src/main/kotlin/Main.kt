76148e013d47fc392855f5ead3ae6fb2af43a617
Hugo Nogueira <hugonogueira055@gmail.com>
2023-10-06 10:40:09 +0100

    commit 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,36 @@
-fun main(args: Array<String>) {
-    var hello = readlnOrNull()
-    print(hello)
+import java.text.DecimalFormat
+import kotlin.math.round
+
+fun main() {
+    println("Welcome to the EcoCalculator!")
+    println("Type 1 if your car runs on Gasoline, type 2 if your car runs on Diesel and type 3 if your car is electric:")
+    val carType = readlnOrNull()
+    println("Please choose the distance you want to travel in Km:")
+    val distanceKm = readln().toInt()
+    println("Finally, what is the average consumption of your vehicle?")
+    val averageConsumption = readln().toFloat()
+    val df = DecimalFormat("#.##")
+    if (carType == "1") {
+        val gasPrice = 1.862
+        val moneySaved = df.format( savings(gasPrice, distanceKm , averageConsumption))
+        println("If you were using an electric car you could have saved $moneySaved €")
+    } else if (carType == "2") {
+        val gasPrice = 1.812
+        val moneySaved= df.format(savings(gasPrice, distanceKm, averageConsumption))
+        println("If you were using an electric car you could have saved $moneySaved €")
+
+    } else if (carType == "3") {
+        println("Good Job! You are already Eco-Friendly!")
+        val electricPrice = df.format(distanceKm *0.0025 * 6.94)
+        print("In that trip you spent a total of $electricPrice €")
+    } else {
+        print("Error, please choose one of the 3 options available")
+    }
+}
+fun savings(gasPrice: Double, distanceKm: Int, averageConsumption: Float): Float {
+    val fuelNeeded = (distanceKm*0.01)*averageConsumption
+    val moneyNeeded = fuelNeeded * gasPrice
+    val electricPrice = (distanceKm *0.0025)* 6.94
+    val saved = (moneyNeeded - electricPrice).toFloat()
+    return saved
 }
